c18c0ebfe8ba43559eb19a19aa5fbdf6d3853a0e
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2022-08-11 00:52:41 +0100

    ...

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -1,17 +1,29 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import React from 'react';
 
-const CustomInput = () => {
+const CustomInput = ({ value, setValue, placeholder }) => {
   return (
     <View style={styles.container}>
-      <TextInput placeholder='Full Name' style={styles.input}/>
+      <TextInput value={value} onChange={setValue} placeholder={placeholder} style={styles.input}/>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {},
-    input: {},
+  container: {
+    backgroundColor: "#FFFFFF",
+    /* It's making the input twice as wide as it's parent. */
+    width: '200%',
+    height: 40,
+
+    borderColor: '#FFFFFF',
+    borderRadius: 10,
+    borderWidth: 1,
+
+    paddingHorizontal: 10,
+    marginVertical: 5,
+  },
+  input: {},
 });
 
 export default CustomInput;
--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,15 +1,34 @@
-import { StyleSheet, View, Image, } from 'react-native';
-import React from 'react';
+import { Text, StyleSheet, View, Image, } from 'react-native';
+import React, {useState} from 'react';
 import Logo from '../../assets/img/FoodieLogoBlack.png';
 import CustomInput from '../components/CustomInput';
 
 const SignUp = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View style={styles.container}>
-      <Image source={Logo} style={styles.logo}/>
-      <CustomInput></CustomInput>
+      <Image source={Logo} style={styles.logo} />
+      <Text style={styles.text}>Full Name</Text>
+      <CustomInput
+        placeholder="Full Name"
+        value=""
+        setValue="setUsername"
+      />
+      <CustomInput
+        placeholder="Email Address"
+        value=""
+        setValue="setEmail"
+      />
+      <CustomInput
+        placeholder="Password"
+        value=""
+        setValue="setPassword"
+      />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -23,6 +42,11 @@ const styles = StyleSheet.create({
   logo: {
     width: '100%',
     height: '20%',
+  },
+  text: {
+    /* Aligning the text to the left. */
+    alignItems: 'flex-start',
+    color: '#FFFFFF'
   },
 });
 
